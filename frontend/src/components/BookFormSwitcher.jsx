4a41c6bef0cc498accf334e2373afd026615bbb0
chsa911 <ch.sa911@gmail.com>
2026-05-30 20:25:43 +0200

    fix: restore mobile PWA form in BookFormSwitcher

--- frontend/src/components/BookFormSwitcher.jsx
+++ frontend/src/components/BookFormSwitcher.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import BookFormDesktop from "./BookFormDesktop";
+import BookFormStagingPwa from "./BookFormStagingPwa";
+
+const isMobileSafari = () => {
+  const ua = navigator.userAgent;
+  return /iP(hone|ad|od)/.test(ua) && /WebKit/.test(ua) && !/CriOS|FxiOS|OPiOS/.test(ua);
+};
 
 export default function BookFormSwitcher(props) {
-  console.log("FORCED DESKTOP BookFormDesktop");
+  if (isMobileSafari()) {
+    return <BookFormStagingPwa {...props} />;
+  }
   return <BookFormDesktop {...props} />;
 }
